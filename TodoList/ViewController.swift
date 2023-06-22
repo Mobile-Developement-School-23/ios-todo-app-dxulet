aa64318e3f38367673da12c8baea06004f9e1d8e
Daulet Ashikbayev <dxulet@gmail.com>
2023-06-22 16:35:08 +0600

    Initial Commit

---
+++ TodoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by Daulet Ashikbayev on 22.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
